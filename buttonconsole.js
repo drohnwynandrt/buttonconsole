59de65bfd3d4aae5c0dfeabcd016f407c34f2214
drohnwynandrt <dw.ramdattewari@gmail.com>
2019-05-06 17:06:25 +0200

    Added buttons with a console log function

---
+++ buttonconsole.js
@@ -0,0 +1,20 @@
+// var buttonone = document.getElementById('buttonone');
+
+
+function logFunctieOne(){
+
+    console.log("1");
+}
+
+function logFunctieSeven(){
+
+    console.log("7");
+}
+
+function plusFunctie(){
+   var plusFunctie = logFunctieSeven+logFunctieOne;
+   console.log(plusFunctie)
+
+}
+
+// console.log(buttonone);
