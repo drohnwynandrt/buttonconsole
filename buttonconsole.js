0742c35b7728a6600a901f69d9dbb5f0fc5fc1bc
drohnwynandrt <dw.ramdattewari@gmail.com>
2019-05-07 10:39:24 +0200

    Added eval function to calculate the data from the input/output

--- buttonconsole.js
+++ buttonconsole.js
@@ -1,9 +1,34 @@
 // var buttonone = document.getElementById('buttonone');
 function logFunctieOne(){
-
-
 document.getElementById("output").innerHTML += "1";
-
 }
+function logFunctieTwo(){
+    document.getElementById("output").innerHTML += "2";
+ }
+ function logFunctieThree(){
+    document.getElementById("output").innerHTML += "3";
+ }
+ function logFunctieFour(){
+    document.getElementById("output").innerHTML += "4";
+ }
+function logFunctieSeven(){
+    document.getElementById("output").innerHTML += "7";
+ }
+ function plusFunctie(){
+    document.getElementById("output").innerHTML += "+";
+ }
 
+ function equalsFunctie(){
+     var result = eval(document.getElementById("output").innerHTML);
+    document.getElementById("output").innerHTML = result;
+ }
+
+ //var a = document.getElementById("output");
+ //var b = ;
+ //var result = a+b;
+
+
+// function resultFunctie(){
+//    document.getElementById("result").innerHTML = result;}
+//     document.getElementById("output").innerHTML = '';
 // console.log(buttonone);
